41dceaf380b40e0bf09204d72e48c573db16a284
Danielle Emrich <danielle@typeislife.com>
2022-01-26 18:05:43 -0700

    parallax beginnings

--- Assets/Scripts/MoveToMouse.cs
+++ Assets/Scripts/MoveToMouse.cs
@@ -7,17 +7,25 @@ using UnityEngine.InputSystem;
 public class MoveToMouse : MonoBehaviour
 {
     public float minimize = 0.03f;
-
     protected Camera mainCam;
     protected float z;
     protected Vector2 basePosition;
 
+    
     protected virtual void Awake()
     {
         mainCam = Camera.main;
         var dist = transform.position - mainCam.transform.position;
         z = Distance(transform.position.z, mainCam.transform.position.z);
         basePosition = transform.position;
+        
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        if (context.canceled) return;
+        Vector2 mousePos = context.ReadValue<Vector2>();
+        MoveMouse(mousePos);
     }
 
     protected virtual void MoveMouse(Vector2 mousePos)
@@ -30,24 +38,23 @@ public class MoveToMouse : MonoBehaviour
         transform.position = new Vector3(basePosition.x, basePosition.y, 0) +
                              new Vector3(newPos.x, newPos.y, transform.position.z);
     }
-
-    protected float CalculateX(float mouseX)
-    {
-        return mouseX * minimize * ((float) Screen.height / (float) Screen.width);
-    }
     
-    protected float CalculateY(float mouseY)
-    {
-        return mouseY * minimize * ((float) Screen.width / (float) Screen.height);
-    }
-
     private bool OutOfBounds()
     {
         Vector2 mousePos = Mouse.current.position.ReadValue();
         if (mousePos.x < 0 || mousePos.y < 0 || mousePos.x > Screen.width || mousePos.y > Screen.height) return true;
         return false;
     }
-    
+
+    protected float CalculateX(float mouseX)
+    {
+        return mouseX * minimize * ((float) Screen.height / (float) Screen.width);
+    }
+    protected float CalculateY(float mouseY)
+    {
+        return mouseY * minimize * ((float) Screen.width / (float) Screen.height);
+    }
+
     protected float Distance(float a, float b)
     {
         return Mathf.Abs(a - b);
